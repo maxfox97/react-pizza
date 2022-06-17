4524f6b036082e261d5413224e74bca286308909
maxfox97 <max.lisicin@gmail.com>
2022-06-17 09:36:47 +0300

    les6-reactPizza

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({data}) => {
+      setPizzas(data.pizzas)
+    })
+  }, [])
+
+  console.log(pizzas)
+
   return (
     <div className="wrapper">
       <Header  />
       <div className="content">
-      <Home />
+      <Home items={pizzas}/>
       </div>
     </div>
   );
